93c0e0edf87e2829eb5f92038bbb43c90f3e50e3
RenasSitdikov <renas.sitdikov@flatstack.com>
2019-11-04 15:25:27 +0300

    solved

--- src/components/List.js
+++ src/components/List.js
@@ -8,21 +8,33 @@ const List = () => {
     function addTimer() {
         const addedTimers = [...timers];
         addedTimers.push({
-            onEnd: (index) => {
-                const removedTimers = [...timers];
-                removedTimers.splice(index, 1);
-                setTimers(removedTimers);
-            },
             timer: 5,
             index: timers.length,
+            finished: false
         });
         setTimers(addedTimers);
     }
 
+    const onEnds = index => {
+        const changedTimers = [...timers];
+        changedTimers[index].finished = true;
+        setTimers(changedTimers);
+    };
+
+    const onIncr = (index, val) => {
+        if (val < 0) {
+            onEnds(index);
+        } else {
+            const changedTimers = [...timers];
+            changedTimers[index].timer = val;
+            setTimers(changedTimers);
+        }
+    };
+
     return (
         <>
             <Button variant="contained" onClick={addTimer}>Add</Button>
-            {timers.map((timerProps, index) => <Timer key={index} { ...timerProps }/>)}
+            {timers.map((timerProps, index) => !timerProps.finished ? <Timer key={index} { ...timerProps } onEnds={onEnds} onIncr={onIncr}/> : null)}
         </>
     );
 };
